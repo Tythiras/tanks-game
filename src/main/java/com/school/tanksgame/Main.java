683daeed7edff0b1221d89876e2a822a62c84169
initzx <initzx>
2019-09-16 11:17:01 +0200

    Added pom.xml and base main class

---
+++ src/main/java/com/school/tanksgame/Main.java
@@ -0,0 +1,24 @@
+package com.school.tanksgame;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("com.school.tanksgame.Main");
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+    }
+
+    @Override
+    public void setup() {
+        super.setup();
+    }
+
+    @Override
+    public void draw() {
+        super.draw();
+    }
+}
