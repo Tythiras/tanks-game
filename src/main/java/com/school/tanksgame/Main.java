0ba985e12571793e039e184d4c6e4907b9fe4f6e
Rasmus Tobiasen <rasmus.tobiasen@bbsure.dk>
2019-09-17 09:43:44 +0200

    #2 Basic dynamics for tank, basic controller for single tank in main

--- src/main/java/com/school/tanksgame/Main.java
+++ src/main/java/com/school/tanksgame/Main.java
@@ -1,12 +1,13 @@
 package com.school.tanksgame;
 
 import processing.core.PApplet;
+import processing.core.PVector;
 
 public class Main extends PApplet {
     public static void main(String[] args) {
         PApplet.main("com.school.tanksgame.Main");
     }
-
+    Tank tank;
     @Override
     public void settings() {
         size(500, 500);
@@ -14,11 +15,50 @@ public class Main extends PApplet {
 
     @Override
     public void setup() {
+        tank = new Tank(this, new PVector(width/3, height/2));
 
     }
 
     @Override
     public void draw() {
-        rect(width/2, height/2, 50,50);
+        clear();
+
+        tank.update();
+        tank.draw();
     }
+
+    @Override
+    public void keyPressed() {
+        //right arrow key
+        if (keyCode == 39) {
+            tank.isRotatingUp = true;
+        //left arrow key
+        } else if (keyCode == 37) {
+            tank.isRotatingDown = true;
+        //up arrow key
+        } else if (keyCode == 38) {
+            tank.isDrivingForward = true;
+        //down arrow key
+        } else if (keyCode == 40) {
+            tank.isDrivingBackwards = true;
+        }
+    }
+
+    @Override
+    public void keyReleased() {
+        //right arrow key
+        if (keyCode == 39) {
+            tank.isRotatingUp = false;
+            //left arrow key
+        } else if (keyCode == 37) {
+            tank.isRotatingDown = false;
+            //up arrow key
+        } else if (keyCode == 38) {
+            tank.isDrivingForward = false;
+            //down arrow key
+        } else if (keyCode == 40) {
+            tank.isDrivingBackwards = false;
+        }
+    }
+
 }
--- src/main/java/com/school/tanksgame/Tank.java
+++ src/main/java/com/school/tanksgame/Tank.java
@@ -1,13 +1,80 @@
 package com.school.tanksgame;
 
+import processing.core.PApplet;
+import processing.core.PConstants;
 import processing.core.PVector;
 
 public class Tank {
+    PApplet p;
+
     PVector location;
     float rotation;
+    float drivingVelocity = 3;
+    float rotationVelocity = (float) 0.05;
 
-    public Tank(PVector location) {
-            this.location = location;
-            rotation = 0;
+    boolean isRotatingUp = false;
+    boolean isRotatingDown = false;
+    boolean isDrivingForward = false;
+    boolean isDrivingBackwards = false;
+
+    float width = 50;
+    float height = 20;
+
+    public Tank(PApplet parent, PVector location) {
+        p = parent;
+
+        this.location = location;
+        rotation = 0;
+    }
+
+    void update() {
+        //update rotation
+        if (isRotatingUp) {
+            rotation += rotationVelocity;
+            if (rotation > 2*Math.PI) {
+                rotation = 0;
+            }
+        }
+        if (isRotatingDown) {
+            rotation -= rotationVelocity;
+            if (rotation < 0) {
+                rotation = (float) (2*Math.PI);
+            }
+        }
+
+        //update location and make sure it isn't over the window boundaries
+        PVector newLoc = new PVector(location.x, location.y);
+        PVector dirVec = PVector.fromAngle(rotation);
+        dirVec.setMag(drivingVelocity);
+
+        if (isDrivingForward) {
+            newLoc.add(dirVec);
+        }
+        if (isDrivingBackwards) {
+            dirVec.mult(-1);
+            newLoc.add(dirVec);
+        }
+        p.println(newLoc);
+        if(newLoc.x < p.width && newLoc.x > 0 && newLoc.y < p.height && newLoc.y > 0 ) {
+            location = newLoc;
+        }
+    }
+
+    void draw() {
+        p.pushMatrix();
+        p.translate(location.x, location.y);
+        p.rotate(rotation);
+
+        p.rectMode(PConstants.CENTER);
+        //main body
+        p.rect(0, 0, width, height);
+
+        //cannon shaft
+        float shaftWidth = (float) (width / 1.25);
+        float shaftHeight = height / 5;
+        p.rect(shaftWidth / 2 + width / 2, 0, shaftWidth, shaftHeight);
+
+
+        p.popMatrix();
     }
 }
