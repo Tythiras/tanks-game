4ce65f2cafedd743e3f2ee91ab03597bba1c1a72
Rasmus Tobiasen <rasmus.tobiasen@bbsure.dk>
2019-09-19 13:34:37 +0200

    #10 Added corners as hitbox so it can drive all the up of walls

--- src/main/java/com/school/tanksgame/Constants.java
+++ src/main/java/com/school/tanksgame/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 
     public static final float TANK_DRIVING_VEL = 3;
     public static final float TANK_ROTATIONAL_VEL = 0.05f;
-    public static final float TANK_WIDTH = 30;
+    public static final float TANK_WIDTH = 60;
     public static final float TANK_HEIGHT = 30;
     public static final float TANK_SHAFT_HEIGHT = 20;
     public static final float TANK_SHAFT_WIDTH = 5;
